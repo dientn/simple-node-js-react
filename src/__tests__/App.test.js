4bd3dae1e41269d65f95ebfcc172f3134df12352
saobangpro2991 <tranngocdien2991@gmail.com>
2018-06-20 17:55:32 +0700

    aa

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,7 +1,9 @@
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// import 'raf/polyfill';
+import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
